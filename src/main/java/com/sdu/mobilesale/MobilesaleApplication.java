809ccc7aedda030742fad91150e46cb4060d1419
GaoLi <gaoli199512@163.com>
2018-03-10 09:51:59 +0800

    init Project

---
+++ src/main/java/com/sdu/mobilesale/MobilesaleApplication.java
@@ -0,0 +1,12 @@
+package com.sdu.mobilesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilesaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilesaleApplication.class, args);
+	}
+}
